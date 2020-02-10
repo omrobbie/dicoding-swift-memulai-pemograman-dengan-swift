bb73ad604b87c24d4796d64df2ce1c881f95f2e8
omrobbie <putu.roby@yahoo.com>
2020-02-10 12:03:50 +0800

    tampilkan data konstan dari perhitungan balok

--- MenghitungBalok/MenghitungBalok/main.swift
+++ MenghitungBalok/MenghitungBalok/main.swift
@@ -8,5 +8,20 @@
 
 import Foundation
 
-print("Hello, World!")
+let width = 2.0
+let height = 3.0
+let length = 4.0
 
+let volume = length * height * width
+let surfaceArea = 2 * ((width * length) + (width * height) + (height * length))
+let circumference = 4 * (width + length + height)
+
+print("----------------------------------")
+print("Anda memiliki sebuah balok dengan:")
+print("Lebarnya adalah \(width) cm")
+print("Tingginya adalah \(height) cm")
+print("Panjangnya adalah \(length) cm")
+print("Volemenya adalah \(volume) cm3")
+print("Luas permukaannya adalah \(surfaceArea) cm2")
+print("Kelilingnya adalah \(circumference) cm")
+print("----------------------------------")
