6484c638d963c620cd2f5d9d24401b304b35009f
omrobbie <putu.roby@yahoo.com>
2020-02-10 12:16:58 +0800

    * mulai project resep makanan *

---
+++ ResepMakanan/ResepMakanan/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ResepMakanan
+//
+//  Created by omrobbie on 10/02/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
