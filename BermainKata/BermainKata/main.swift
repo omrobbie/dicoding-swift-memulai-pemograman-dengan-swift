ba26715f74568e3a5441ef7f10268be3e33f1918
omrobbie <putu.roby@yahoo.com>
2020-02-10 12:11:22 +0800

    pengolahan data string

--- BermainKata/BermainKata/main.swift
+++ BermainKata/BermainKata/main.swift
@@ -8,5 +8,64 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Selamat Datang di Dicoding Academy!")
 
+print("Mari bermain kata!")
+print("----------------------------------")
+
+print("Masukkanlah nama depan Anda:"); let firstName = String(readLine() ?? "")
+print("Masukkanlah nama belakang Anda:"); let lastName = String(readLine() ?? "")
+print("----------------------------------")
+print("Berikut ini adalah operator-operator yang digunakan")
+
+let concatenationString = firstName + lastName
+print("String Concatenation Operator : \"\(concatenationString)\"")
+
+var mutabilityString = ""
+mutabilityString += firstName
+mutabilityString += lastName
+print("String Mutability Operator : \"\(mutabilityString)\"")
+
+// Operator Comparison
+var comparisonString = ""
+if firstName == lastName {
+    comparisonString = String(true)
+} else {
+    comparisonString = String(false)
+}
+print("String Comparison Operator : \"\(comparisonString)\"")
+print("----------------------------------")
+print("Berikut ini adalah fungsi-fungsi yang digunakan")
+
+let fullName = firstName + " " + lastName
+
+let empty = firstName.isEmpty || lastName.isEmpty
+print("Fungsi isEmpty : \"\(empty)\"")
+
+let startIndex = fullName[fullName.startIndex]
+print("Fungsi startIndex : \"\(startIndex)\"")
+
+let index = fullName[fullName.index(after: fullName.startIndex)]
+print("Fungsi index : \"\(index)\"")
+
+let customIndex = fullName[fullName.index(fullName.startIndex, offsetBy: 5)]
+print("Index ke lima : \"\(customIndex)\"")
+
+let endIndex = fullName[fullName.index(before: fullName.endIndex)]
+print("Fungsi endIndex : \"\(endIndex)\"")
+
+var insert = fullName
+insert.insert("!", at:insert.endIndex)
+print("Fungsi insert : \"\(insert)\"")
+
+var remove = insert
+remove.remove(at: remove.index(before: remove.endIndex))
+print("Fungsi remove : \"\(remove)\"")
+
+var append = firstName
+append.append(lastName)
+print("Fungsi append : \"\(append)\"")
+
+let count = fullName.count
+print("Fungsi lenght : \(count)")
+print("----------------------------------")
