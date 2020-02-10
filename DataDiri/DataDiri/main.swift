dd807c6b1534c3d5fb2c60c8c815e4080da0df1a
omrobbie <putu.roby@yahoo.com>
2020-02-10 11:44:17 +0800

    tampilkan data konstan di  command line

--- DataDiri/DataDiri/main.swift
+++ DataDiri/DataDiri/main.swift
@@ -8,5 +8,15 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Selamat Datang di Dicoding Academy")
+let firstName = "Putu", lastName = "Roby"
+let fullName = firstName + " " + lastName
+let address = "Denpasar"
+let job = "iOS Developer"
+let age = 0b100101
 
+print("----------------------------------")
+print("Apakah kalian tahu \(fullName)?")
+print("\(firstName) adalah seorang \(job)")
+print("Saat ini ia berumur \(age) tahun dan bertempat tinggal di \(address)")
+print("----------------------------------")
