9f1acbfd97ce01d33d32957b14e50ef3a466b7c6
omrobbie <putu.roby@yahoo.com>
2020-02-10 12:04:58 +0800

    tampilkan data dengan inputan

--- MenghitungBalok/MenghitungBalok/main.swift
+++ MenghitungBalok/MenghitungBalok/main.swift
@@ -8,20 +8,29 @@
 
 import Foundation
 
-let width = 2.0
-let height = 3.0
-let length = 4.0
-
-let volume = length * height * width
-let surfaceArea = 2 * ((width * length) + (width * height) + (height * length))
-let circumference = 4 * (width + length + height)
-
+print("Selamat Datang di Dicoding Academy")
 print("----------------------------------")
-print("Anda memiliki sebuah balok dengan:")
-print("Lebarnya adalah \(width) cm")
-print("Tingginya adalah \(height) cm")
-print("Panjangnya adalah \(length) cm")
-print("Volemenya adalah \(volume) cm3")
-print("Luas permukaannya adalah \(surfaceArea) cm2")
-print("Kelilingnya adalah \(circumference) cm")
-print("----------------------------------")
+
+print("Masukkanlah lebar balok:"); let widthInput = readLine()
+print("Masukkanlah tinggi balok:"); let heightInput = readLine()
+print("Masukkanlah panjang balok:"); let lengthInput = readLine()
+
+if let length = Double(lengthInput ?? "0"), let height = Double(heightInput ?? "0"), let width = Double(widthInput ?? "0"){
+    let volume = length * height * width
+    let surfaceArea = 2 * ((width * length) + (width * height) + (height * length))
+    let circumference = 4 * (width + length + height)
+
+    print("----------------------------------")
+    print("Anda memiliki sebuah balok dengan:")
+    print("Lebarnya adalah \(width) cm")
+    print("Tingginya adalah \(height) cm")
+    print("Panjangnya adalah \(length) cm")
+    print("Volemenya adalah \(volume) cm3")
+    print("Luas permukaannya adalah \(surfaceArea) cm2")
+    print("Kelilingnya adalah \(circumference) cm")
+    print("----------------------------------")
+} else {
+    print("----------------------------------")
+    print("Input tidak valid")
+    print("----------------------------------")
+}
